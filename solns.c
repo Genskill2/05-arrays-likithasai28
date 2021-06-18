efc6458d4d53a861b2436b0c3c46108f22fd6a11
likithasai28 <84700248+likithasai28@users.noreply.github.com>
2021-06-18 20:50:03 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 //max function
 int max(int a[], int n){
-int temp=0 ;
+int m=0 ;
  for ( int i = 0; i < n ; i++){
-    if (a[i] > temp){
-         temp = a[i];
+    if (a[i] > m){
+         m = a[i];
         }
       }
   return m;
